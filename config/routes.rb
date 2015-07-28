7ba786bb21b3afa915e90b001c37863d8d9249b8
Dmitry BRANAVITSKI <dima_nado@mail.ru>
2015-07-28 11:29:32 +0300

    bug with DTA

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
-# config/routes.rb
-
 Rails.application.routes.draw do
-  scope '/api' do
-    mount_devise_token_auth_for 'User', at: '/auth'
-    resources :groups, except: [:new, :edit]
+
+  # standard devise routes available at /users
+  devise_for :users
+
+  # token auth routes available at /api/v1/auth
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
   end
+
 end
